8409af8df487c69ba6ca2f83189e7d9039e2d15d
Annie-code-dev <kamauannie80@gmail.com>
2025-09-02 00:26:58 +0300

    first commit

---
+++ Innovation.js
@@ -0,0 +1,16 @@
+const perc= 100
+console.log("Hope is " + perc + "%")
+let talk = [" put God first", "pray hard", "& Faith into action"]
+console.log("Let's" + talk.join(", ") + ".")
+let a = 10, b = 5;
+console.log(a + b); // 15
+console.log(a - b); // 5
+console.log(a * b); // 50
+console.log(a / b); // 2
+console.log(a % b); // 0 (remainder)
+let num = 100
+if (num > 90) {
+    console.log("The number is greater than 90")
+}else{
+    console.log("The number is less than or equal to 90")
+}
